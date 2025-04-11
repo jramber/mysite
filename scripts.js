0b4589a4e3bc7e336eb96691782d9d75ded98b2f
jramber <jdramirezbernal@gmail.com>
2025-04-11 14:44:44 +0200

    fix scroll

--- scripts.js
+++ scripts.js
@@ -1,57 +1,43 @@
-function disableScroll () {
-    document.body.style.overflow = 'hidden';
-}
-
-function enableScroll () {
-    document.body.style.overflow = 'auto';
-}
-
 document.addEventListener('DOMContentLoaded', function() {
+
     const header = document.querySelector('.header-wrapper .header');
     const options_menu_wrapper = document.querySelector('.options-menu-wrapper');
-    const options_menu = document.querySelector('.options-menu');
+    // const options_menu = document.querySelector('.options-menu');
     const options_icon = document.querySelector('.options');
 
     options_menu_wrapper.style.top = header.offsetHeight + 'px';
 
     options_icon.addEventListener('click', function() {
-        options_menu.style.height = window.screen.height - document.querySelector('.header').offsetHeight + 'px';
+        // options_menu.style.height = window.screen.height - document.querySelector('.header').offsetHeight + 'px';
         options_menu_wrapper.classList.toggle('visible');
         options_icon.classList.toggle('active');
 
         if (options_menu_wrapper.classList.contains('visible')) {
-            disableScroll();
+            document.body.style.overflow = 'hidden';
+            return;
         } else {
-            enableScroll();
+            document.body.style.overflow = 'auto';
+            return;
         }
     });
 
-    const navigation_links = document.querySelectorAll('.navigation-link');
-    navigation_links.forEach(function(link) {
-        link.addEventListener('click', function() {
-            enableScroll();
+    document.querySelectorAll('.navigation-link').forEach(function(link) {
+        link.addEventListener('click', function(event) {
+            event.preventDefault();
+
+            // element = this;
+            // setTimeout(function() {
+                document.getElementById(this.getAttribute('href').substring(1)).scrollIntoView({ behavior: 'smooth' });
+                history.pushState(null, '', this.getAttribute('href'));
+            // }, 0);
+
+            document.body.style.overflow = 'auto';
+            options_icon.classList.remove('active');
             options_menu_wrapper.classList.remove('visible');
-            options_icon.classList.toggle('active');
         });
-    })
+        return;
+    });
 
-    /*
-        const navbar = document.querySelector('.navbar');
-        const offset = document.querySelector('.offset');
-
-        navbar.addEventListener('click', function() {
-            offset.classList.toggle('visible');
-
-            if (offset.classList.contains('visible')) {
-                disableScroll();
-            } else {
-                enableScroll();
-            }
-        });
-
-        // do your setup here
-        console.log('Initialized app');
-        // Initialize the app
-    */
+    return;
 });
 
